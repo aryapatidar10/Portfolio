13100434b2cdf757035971e0b66a87f6fbf25598
Arya Patidar <arya.patidar2003@gmail.com>
2024-06-29 11:14:31 +0530

    commit

---
+++ src/components/Home/AboutMe.jsx
@@ -0,0 +1,21 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/img_1-remove-bg.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+          I'm a pre-final year web developer with expertise in HTML, CSS, Bootstrap, JavaScript, and SQL. Passionate about creating user-friendly and responsive websites.
+          </p>
+          <p className="hero--section-description">
+           I've honed my skills through practical projects and a strong foundation in data structures and algorithms. Looking forward to contributing to innovative web solutions and collaborating with like-minded professionals in the industry.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/Home/HeroSection.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-scroll'; // Import Link from react-scroll
+
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Arya</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span> <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            I'm a pre-final year web developer with expertise in HTML, CSS, Bootstrap, JavaScript, and SQL, React.
+            Passionate about creating user-friendly and responsive websites.
+          </p>
+        </div>
+        <Link
+          to="Contact"
+          smooth={true}
+          offset={-70}
+          duration={500}
+          className="btn btn-primary"
+        >
+          Get In Touch
+        </Link>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/img_1-remove-bg.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
